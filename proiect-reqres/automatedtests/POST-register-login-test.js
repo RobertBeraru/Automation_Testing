ac9728532ef449a61145126832b1d8cb5edd3ae5
Beraru Robert <robert_beraru@yahoo.com>
2023-10-30 16:03:27 +0200

    Add changes to POST-register-login-test.js

--- proiect-reqres/automatedtests/POST-register-login-test.js
+++ proiect-reqres/automatedtests/POST-register-login-test.js
@@ -1,9 +1,14 @@
 const {spec, request} = require ('pactum');
 const baseURL = "https://reqres.in/";
 
-const userCred ={
+const userCredRegister ={
     "email": "eve.holt@reqres.in",
     "password": "pistol"
+}
+
+const userCredLogIn ={
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
 }
 
 describe('POST Register and login user test', () => {
@@ -13,7 +18,7 @@ describe('POST Register and login user test', () => {
     it('POST Register user test',async () => {
         await spec()
         .post(baseURL+'api/register')
-        .withBody (userCred)
+        .withBody (userCredRegister)
         .withHeaders('Content-Type', 'application/json')
         .expectStatus(200)
     });
@@ -23,7 +28,7 @@ describe('POST Register and login user test', () => {
     it('POST Login user test',async () => {
         await spec()
         .post(baseURL+'api/login')
-        .withBody(userCred)
+        .withBody(userCredLogIn)
         .withHeaders('Content-Type', 'application/json')
         .expectStatus(200)
         
